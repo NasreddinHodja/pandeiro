6ef65548515876e97219a9edbd034a3563aee594
tbizetde@gmail.com <tbizetde@gmail.com>
2025-04-21 22:06:37 -0300

    button darken when active

--- components/Shared/Button.vue
+++ components/Shared/Button.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex border-2 border-black transition-all duration-75 hover:border-white">
+  <div
+    class="flex border-2 border-black transition-all duration-75 hover:border-white active:bg-gray-200"
+  >
     <slot />
   </div>
 </template>

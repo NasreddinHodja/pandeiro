83389503b7873f7ace0f344dc90655d303b52192
tbizetde@gmail.com <tbizetde@gmail.com>
2025-05-12 23:37:46 -0300

    increment and decrement buttons added to SharedNumberSlider

--- components/Shared/GlobalMetronome.vue
+++ components/Shared/GlobalMetronome.vue
@@ -14,14 +14,17 @@ onMounted(() => {
     </template>
 
     <Transition appear name="fade-and-rise">
-      <div v-if="mounted" class="flex flex-col gap-4 lg:p-12 p-6 text-white border-4 border-white">
+      <div v-if="mounted" class="flex flex-col gap-8 lg:p-12 p-6 text-white border-4 border-white">
         <div class="flex items-center gap-6">
-          <SharedPlayButton :is-playing="isPlaying" @toggle="toggle" />
+          <SharedPlayButton :is-playing="isPlaying" class="hidden md:block" @toggle="toggle" />
 
           <div class="flex flex-col gap-4 w-full">
             <SharedNumberSlider v-model:value="bpm" label="BPM" :min="40" :max="240" />
             <SharedNumberSlider v-model:value="volume" label="Volume" :min="0" :max="100" />
           </div>
+        </div>
+        <div class="flex justify-center md:hidden">
+          <SharedPlayButton :is-playing="isPlaying" @toggle="toggle" />
         </div>
       </div>
     </Transition>
--- components/Shared/NumberSlider.vue
+++ components/Shared/NumberSlider.vue
@@ -1,27 +1,56 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   label: string;
   min: number;
   max: number;
 }>();
 
 const value = defineModel<number>("value");
+
+const increment = () => {
+  if (value.value < props.max) value.value += 1;
+};
+
+const decrement = () => {
+  if (value.value > props.min) value.value -= 1;
+};
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 w-full sm:flex-row sm:items-center">
+  <div class="flex gap-1 md:gap-4 w-full flex-col md:flex-row">
     <div class="flex items-center gap-3 justify-between min-w-28">
       <span>{{ label }}:</span>
       <span class="inline-block text-right tabular-nums w-[3ch]">{{ value }}</span>
     </div>
 
-    <input
-      v-model.number="value"
-      type="range"
-      :min="min"
-      :max="max"
-      step="1"
-      class="w-full h-2 bg-white/20 appearance-none accent-white [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-3 [&::-webkit-slider-thumb]:h-3 [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:border-none [&::-webkit-slider-thumb]:shadow-none [&::-webkit-slider-thumb]:rounded-none [&::-moz-range-thumb]:w-3 [&::-moz-range-thumb]:h-3 [&::-moz-range-thumb]:bg-white [&::-moz-range-thumb]:border-none [&::-moz-range-thumb]:rounded-none"
-    />
+    <div class="flex flex-col sm:flex-row sm:flex-wrap md:flex-nowrap items-center gap-3 w-full">
+      <div class="flex gap-3 w-full md:w-auto justify-between">
+        <SharedButton class="bg-white text-black p-1 md:p-2 flex items-center" @click="decrement">
+          <Icon name="mdi:minus" class="text-sm md:text-base" />
+        </SharedButton>
+        <SharedButton
+          class="bg-white text-black p-1 md:p-2 md:hidden flex items-center"
+          @click="increment"
+        >
+          <Icon name="mdi:plus" />
+        </SharedButton>
+      </div>
+
+      <input
+        v-model.number="value"
+        type="range"
+        :min="min"
+        :max="max"
+        step="1"
+        class="order-2 md:order-none w-full h-2 bg-white/20 appearance-none accent-white [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-3 [&::-webkit-slider-thumb]:h-3 [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:border-none [&::-webkit-slider-thumb]:shadow-none [&::-webkit-slider-thumb]:rounded-none [&::-moz-range-thumb]:w-3 [&::-moz-range-thumb]:h-3 [&::-moz-range-thumb]:bg-white [&::-moz-range-thumb]:border-none [&::-moz-range-thumb]:rounded-none"
+      />
+
+      <SharedButton
+        class="bg-white text-black p-1 md:p-2 hidden md:flex flex items-center"
+        @click="increment"
+      >
+        <Icon name="mdi:plus" />
+      </SharedButton>
+    </div>
   </div>
 </template>
